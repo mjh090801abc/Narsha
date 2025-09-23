1ea78cf1d1dce8dd2d4978c8e90a73c0b5605fb3
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-09-23 19:34:06 +0900

    test commit

---
+++ src/main/java/com/spring/dishcovery/DishcoveryApplication.java
@@ -0,0 +1,13 @@
+package com.spring.dishcovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishcoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DishcoveryApplication.class, args);
+    }
+
+}
