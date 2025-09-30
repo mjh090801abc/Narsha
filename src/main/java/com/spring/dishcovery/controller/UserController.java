32a1411dae63f490ebe230c6e736c5670e471673
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-09-30 17:38:04 +0900

    login 페이지 불러오기 추가

--- src/main/java/com/spring/dishcovery/controller/UserController.java
+++ src/main/java/com/spring/dishcovery/controller/UserController.java
@@ -24,6 +24,9 @@ public class UserController {
         return "redirect:/login";
     }
 
-
+    @GetMapping("/dishcovery_login")
+    public String dishcoveryLogin() {
+        return "html/login";
+    }
 
 }
