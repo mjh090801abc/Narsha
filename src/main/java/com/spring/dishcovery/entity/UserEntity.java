733c0d5ae305cb73437010e3506b1bf1252d5026
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-10-21 18:03:09 +0900

    이메일, 이름, 닉네임, 비밀번호 DB에 맞게 코드 수정

--- src/main/java/com/spring/dishcovery/entity/UserEntity.java
+++ src/main/java/com/spring/dishcovery/entity/UserEntity.java
@@ -8,11 +8,11 @@ import lombok.Setter;
 
 public class UserEntity {
 
-    private String userID;
+    private String userMail;
 
-    private String username;
+    private String userName;
 
-    private String password;
+    private String userNickName;
 
-    private String email;
+    private String userPSWD;
 }
