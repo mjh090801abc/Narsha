ee224f0960e4d298c6b37ea59e8d599448803c94
dgsw <aaa000001@naver.com>
2025-10-28 22:53:14 +0900

    web config 자리이동

--- src/main/java/com/spring/dishcovery/config/WebConfig.java
+++ src/main/java/com/spring/dishcovery/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.spring.config;
+package com.spring.dishcovery.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
