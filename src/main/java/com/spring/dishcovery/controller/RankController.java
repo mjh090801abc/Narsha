3407e2b4129cda0d7e1f78534bbc6cfc913a723f
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-11-25 20:14:59 +0900

    @RestController -> @Controller 수정 html로 리턴 + 메서드명 수정

--- src/main/java/com/spring/dishcovery/controller/RankController.java
+++ src/main/java/com/spring/dishcovery/controller/RankController.java
@@ -3,22 +3,25 @@ package com.spring.dishcovery.controller;
 import com.spring.dishcovery.entity.RecipeVo;
 import com.spring.dishcovery.service.RecipeAppService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class RankController {
 
     private final RecipeAppService recipeAppService;
 
     @GetMapping("/Rank")
-    public List<RecipeVo> Rank(Model model) {
+    public String Rank(Model model) {
         List<RecipeVo> recipes = recipeAppService.getAllRecipes();
 
-        return recipes;
+        model.addAttribute("recipes", recipes);
+
+        return "recipe/RankPage";
     }
 }
